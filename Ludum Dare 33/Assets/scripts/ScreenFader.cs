a734a0acf699e58d56be8c9fb0b58cedfdf6c933
StaNov <stanovec@gmail.com>
2015-08-23 15:26:50 +0200

    refactoring cutscén

--- Ludum Dare 33/Assets/scripts/ScreenFader.cs
+++ Ludum Dare 33/Assets/scripts/ScreenFader.cs
@@ -3,6 +3,9 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class ScreenFader : MonoBehaviour {
+	
+	private static float FADE_DURATION = 3;
+	private static float WAIT_AFTER_FADE_OUT = 1;
 
 	private Image panel;
 
@@ -20,7 +23,7 @@ public class ScreenFader : MonoBehaviour {
 	
 	private IEnumerator fadeInCoroutine() {		
 		while (Time.time <= 3) {
-			panel.color = Color.Lerp(Color.black, Color.clear, Time.time / 3);
+			panel.color = Color.Lerp(Color.black, Color.clear, Time.time / FADE_DURATION);
 			yield return null;
 		}
 	}
@@ -28,12 +31,12 @@ public class ScreenFader : MonoBehaviour {
 	private IEnumerator fadeOutAndLoadNextLevelCoroutine() {
 		float timeOfFadeOutStart = Time.time;
 		
-		while (Time.time <= timeOfFadeOutStart + 3) {
-			panel.color = Color.Lerp(Color.clear, Color.black, (Time.time - timeOfFadeOutStart) / 3);
+		while (Time.time <= timeOfFadeOutStart + FADE_DURATION) {
+			panel.color = Color.Lerp(Color.clear, Color.black, (Time.time - timeOfFadeOutStart) / FADE_DURATION);
 			yield return null;
 		}
 
-		yield return new WaitForSeconds(1);
+		yield return new WaitForSeconds(WAIT_AFTER_FADE_OUT);
 		
 		Application.LoadLevel(Application.loadedLevel + 1);
 	}
