ea9917d4a5d96eae88ca402a103a6f90db64d094
StaNov <stanovec@gmail.com>
2015-08-23 16:19:29 +0200

    zvuk cutscény

--- Ludum Dare 33/Assets/scripts/ScreenFader.cs
+++ Ludum Dare 33/Assets/scripts/ScreenFader.cs
@@ -5,30 +5,37 @@ using System.Collections;
 public class ScreenFader : MonoBehaviour {
 	
 	private static float FADE_DURATION = 3;
-	private static float WAIT_AFTER_FADE_OUT = 1;
+	private static float WAIT_AFTER_FADE_OUT = 0.5f;
 
 	private Image panel;
+	private AudioClip audioClip;
+	private Animator animator;
 
 	void Start () {
 		panel = GetComponentInChildren<Image>();
+		audioClip = GetComponent<AudioSource>().clip;
+		animator = GetComponent<Animator>();
 		panel.color = Color.black;
+		animator.speed = 1/audioClip.length;
 
-		StartCoroutine(fadeInCoroutine());
-	}
-
-
-	public void fadeOutAndLoadNextLevel() {
-		StartCoroutine(fadeOutAndLoadNextLevelCoroutine());
+		StartCoroutine(fadeInPlayAnimationFadeOut());
 	}
 	
-	private IEnumerator fadeInCoroutine() {		
+
+	private IEnumerator fadeInPlayAnimationFadeOut() {
+
+		// fade in
 		while (Time.time <= 3) {
 			panel.color = Color.Lerp(Color.black, Color.clear, Time.time / FADE_DURATION);
 			yield return null;
 		}
-	}
-	
-	private IEnumerator fadeOutAndLoadNextLevelCoroutine() {
+
+
+		// wait for audioClip end
+		yield return new WaitForSeconds(audioClip.length - 2*FADE_DURATION);
+
+
+		// fade out
 		float timeOfFadeOutStart = Time.time;
 		
 		while (Time.time <= timeOfFadeOutStart + FADE_DURATION) {
