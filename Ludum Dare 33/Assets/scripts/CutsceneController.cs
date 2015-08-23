0954dfa0c8519698264697500cb1b5b72a6baf5a
StaNov <stanovec@gmail.com>
2015-08-23 15:07:50 +0200

    první cutscéna, fadein/fadeout

---
+++ Ludum Dare 33/Assets/scripts/CutsceneController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutsceneController : MonoBehaviour {
+
+	public ScreenFader screenFader;
+
+	public void FadeOutAndLoadNextLevel() {
+		screenFader.fadeOutAndLoadNextLevel();
+	}
+}
---
+++ Ludum Dare 33/Assets/scripts/ScreenFader.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScreenFader : MonoBehaviour {
+
+	private Image panel;
+
+	void Start () {
+		panel = GetComponentInChildren<Image>();
+		panel.color = Color.black;
+
+		StartCoroutine(fadeInCoroutine());
+	}
+
+
+	public void fadeOutAndLoadNextLevel() {
+		StartCoroutine(fadeOutAndLoadNextLevelCoroutine());
+	}
+	
+	private IEnumerator fadeInCoroutine() {		
+		while (Time.time <= 3) {
+			panel.color = Color.Lerp(Color.black, Color.clear, Time.time / 3);
+			yield return null;
+		}
+	}
+	
+	private IEnumerator fadeOutAndLoadNextLevelCoroutine() {
+		float timeOfFadeOutStart = Time.time;
+		
+		while (Time.time <= timeOfFadeOutStart + 3) {
+			panel.color = Color.Lerp(Color.clear, Color.black, (Time.time - timeOfFadeOutStart) / 3);
+			yield return null;
+		}
+
+		yield return new WaitForSeconds(1);
+		
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
