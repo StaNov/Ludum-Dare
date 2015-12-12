0e1bf4949b97fb028fd6d37a9b3cb6de48a084cd
StaNov <stanovec@gmail.com>
2015-12-12 13:43:04 +0100

    bees controlling

--- Ludum Dare 34/Ludum Dare 34/Assets/PlayerController.cs
+++ Ludum Dare 34/Ludum Dare 34/Assets/PlayerController.cs
@@ -3,25 +3,20 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	private Vector3 targetPosition = Vector3.zero;
+	public Transform targetPosition;
 
 	public int playerSpeed = 10;
 
-	void Update () {
-		if (Input.GetMouseButton(0)) {
-			Vector3 mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			targetPosition = new Vector3(mousePoint.x, mousePoint.y, transform.position.z);
-		} else {
-			targetPosition = Vector3.zero;
-		}
+	void Start() {
+		targetPosition = GameObject.FindWithTag("PlayerTarget").transform;
 	}
 
 	void FixedUpdate() {
-		if (targetPosition == Vector3.zero) {
+		if (targetPosition.position == Vector3.zero) {
 			return;
 		}
 
-		Vector3 moveDirection = targetPosition - transform.position;
+		Vector3 moveDirection = targetPosition.position - transform.position;
 		moveDirection.Normalize();
 
 		transform.Translate(moveDirection * playerSpeed * Time.deltaTime);
