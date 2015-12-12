7883cdca19ccddc99a03dd1d7d29d4aeed5d5595
StaNov <stanovec@gmail.com>
2015-12-12 14:00:49 +0100

    enemy healthbar

--- Ludum Dare 34/Ludum Dare 34/Assets/EnemyLife.cs
+++ Ludum Dare 34/Ludum Dare 34/Assets/EnemyLife.cs
@@ -6,9 +6,11 @@ public class EnemyLife : MonoBehaviour {
 	public int startLife = 100;
 
 	private int currentLife;
+	private Transform healthBar;
 
 	void Start() {
 		currentLife = startLife;
+		healthBar = transform.Find("healthBar").transform;
 	}
 
 	public void Hurt() {
@@ -16,5 +18,6 @@ public class EnemyLife : MonoBehaviour {
 		if (currentLife <= 0) {
 			Destroy(gameObject);
 		}
+		healthBar.localScale = new Vector3((float) currentLife / startLife, healthBar.localScale.y, healthBar.localScale.z);
 	}
 }
