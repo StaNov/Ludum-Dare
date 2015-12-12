6dc0497c03fad5d9f9cd897d33661768704ce210
StaNov <stanovec@gmail.com>
2015-12-12 14:38:01 +0100

    rescuable bees

---
+++ Ludum Dare 34/Ludum Dare 34/Assets/CapturedBee.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CapturedBee : MonoBehaviour {
+
+	private Transform beesParent;
+	public GameObject beePrefab;
+
+	void Start () {
+		beesParent = GameObject.Find("bees").transform;
+	}
+
+	void OnTriggerEnter2D (Collider2D col) {
+		if (!col.CompareTag("Player")) {
+			return;
+		}
+
+		GameObject bee = (GameObject) Instantiate(beePrefab, transform.position, Quaternion.identity);
+		bee.transform.parent = beesParent;
+		Destroy(gameObject);
+	}
+}
