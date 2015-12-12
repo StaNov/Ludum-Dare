2af0c5d90e8a7e46886534de35be40b5aa6b3dc7
StaNov <stanovec@gmail.com>
2015-12-12 14:55:11 +0100

    dummy enemy movement

---
+++ Ludum Dare 34/Ludum Dare 34/Assets/DummyEnemyController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DummyEnemyController : MonoBehaviour {
+
+	public int enemySpeed = 5;
+
+	private bool move = false;
+
+	private Transform player;
+
+	void Start() {
+		player = GameObject.FindWithTag("Player").transform;
+	}
+
+	void FixedUpdate () {
+		if (! move) {
+			return;
+		}
+
+		Vector3 moveVector = (transform.position - player.position);
+		moveVector.Normalize();
+		transform.Translate(moveVector * enemySpeed * Time.deltaTime);
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.collider.CompareTag("Player") || col.collider.CompareTag("Bee")) {
+			move = true;
+		}
+	}
+}
