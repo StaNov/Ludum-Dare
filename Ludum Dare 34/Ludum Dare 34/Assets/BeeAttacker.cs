233cc2303f5353689fbe541461408358f97915e2
StaNov <stanovec@gmail.com>
2015-12-12 13:49:41 +0100

    bee attack

---
+++ Ludum Dare 34/Ludum Dare 34/Assets/BeeAttacker.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeeAttacker : MonoBehaviour {
+
+	void OnCollisionEnter2D (Collision2D col) {
+		if (col.collider.CompareTag("Enemy")) {
+			col.collider.GetComponent<EnemyLife>().Hurt();
+		}
+	}
+}
---
+++ Ludum Dare 34/Ludum Dare 34/Assets/EnemyLife.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyLife : MonoBehaviour {
+
+	public int startLife = 100;
+
+	private int currentLife;
+
+	void Start() {
+		currentLife = startLife;
+	}
+
+	public void Hurt() {
+		currentLife--;
+		if (currentLife <= 0) {
+			Destroy(gameObject);
+		}
+	}
+}
