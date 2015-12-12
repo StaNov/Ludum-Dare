45be5ddd00cbddd0a84fb26e4ca28b8bd1607bff
StaNov <stanovec@gmail.com>
2015-12-12 17:04:59 +0100

    level1

---
+++ Ludum Dare 34/Ludum Dare 34/Assets/scripts/EndLocationManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class EndLocationManager : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (!col.CompareTag("Player")) {
+			return;
+		}
+
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
