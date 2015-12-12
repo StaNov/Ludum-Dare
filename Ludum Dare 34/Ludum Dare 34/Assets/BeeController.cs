6ed445cb9eb391c56f2529fd5cb6efa6e7fbde33
StaNov <stanovec@gmail.com>
2015-12-12 11:11:31 +0100

    beeeees!

---
+++ Ludum Dare 34/Ludum Dare 34/Assets/BeeController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeeController : MonoBehaviour {
+
+	public int beeSpeed = 5;
+
+	private Transform beesTarget;
+	private Rigidbody2D rb;
+
+	void Start () {
+		beesTarget = GameObject.FindGameObjectWithTag("BeesTarget").transform;
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate () {
+		Vector2 myPosition = new Vector2(transform.position.x, transform.position.y);
+		Vector2 playerPosition = new Vector2(beesTarget.position.x, beesTarget.position.y);
+		Vector2 forceVector = playerPosition - myPosition;
+		forceVector.Normalize();
+		rb.AddForce(forceVector * beeSpeed * rb.mass);
+	}
+}
