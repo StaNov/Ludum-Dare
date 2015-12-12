9aaff221ccbf24014863c33080d481c2399dd958
StaNov <stanovec@gmail.com>
2015-12-12 22:04:30 +0100

    menu

---
+++ Ludum Dare 34/Ludum Dare 34/Assets/GameStarter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameStarter : MonoBehaviour {
+
+	public void StartGame() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
