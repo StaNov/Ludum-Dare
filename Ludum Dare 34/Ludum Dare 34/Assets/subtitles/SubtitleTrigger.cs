98cc4fd96fb86d0f9f3d63565f2b604201578a96
StaNov <stanovec@gmail.com>
2015-12-13 13:43:26 +0100

    subtitles

---
+++ Ludum Dare 34/Ludum Dare 34/Assets/subtitles/SubtitleTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SubtitleTrigger : MonoBehaviour {
+
+	private bool visited = false;
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (visited || !col.CompareTag("Player")) {
+			return;
+		}
+
+		Subtitles.GetInstance().ShowNextSubtitle();
+		visited = true;
+	}
+}
---
+++ Ludum Dare 34/Ludum Dare 34/Assets/subtitles/Subtitles.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Subtitles : MonoBehaviour {
+
+	public bool showFirstSubtitleAtStart = false;
+	[Multiline]
+	public string[] subtitles = new string[] {"Test subtitle!\nTest subtitle new line!", "", "", "", ""};
+
+	private GameObject panel;
+	private Text textField;
+
+	private static Subtitles instance;
+
+	private int currentSubtitle = 0;
+
+
+	void Awake() {
+		instance = this;
+	}
+
+	void Start () {
+		panel = transform.GetChild(0).gameObject;
+		textField = panel.transform.GetChild(0).GetComponent<Text>();
+		panel.SetActive(false);
+
+		if (showFirstSubtitleAtStart) {
+			ShowNextSubtitle();
+		}
+	}
+
+	public void ShowNextSubtitle () {
+		if (currentSubtitle >= subtitles.Length) {
+			return;
+		}
+
+		StopAllCoroutines();
+
+		textField.text = subtitles[currentSubtitle];
+		currentSubtitle++;
+		StartCoroutine(HideSubtitle());
+		panel.SetActive(true);
+	}
+
+	private IEnumerator HideSubtitle() {
+		yield return new WaitForSeconds(10);
+		panel.SetActive(false);
+	}
+
+	public static Subtitles GetInstance() {
+		return instance;
+	}
+}
