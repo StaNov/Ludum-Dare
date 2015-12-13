469364553bc6f31fcd677ed48cfab1873002c0e5
StaNov <stanovec@gmail.com>
2015-12-13 16:00:11 +0100

    free bee sound

--- Ludum Dare 34/Ludum Dare 34/Assets/game/scripts/CapturedBee.cs
+++ Ludum Dare 34/Ludum Dare 34/Assets/game/scripts/CapturedBee.cs
@@ -4,10 +4,15 @@ using System.Collections;
 public class CapturedBee : MonoBehaviour {
 
 	private Transform beesParent;
+	private GameObject audioPlayer;
+	private AudioSource audioSource;
+
 	public GameObject beePrefab;
 
 	void Start () {
 		beesParent = GameObject.Find("bees").transform;
+		audioPlayer = transform.GetChild(0).gameObject;
+		audioSource = audioPlayer.GetComponent<AudioSource>();
 	}
 
 	void OnTriggerEnter2D (Collider2D col) {
@@ -17,7 +22,11 @@ public class CapturedBee : MonoBehaviour {
 
 		GameObject bee = (GameObject) Instantiate(beePrefab, transform.position, Quaternion.identity);
 		bee.transform.parent = beesParent;
-		Destroy(gameObject);
 		BeeCountUpdater.GetInstance().UpdateBeeCount();
+
+		audioSource.Play();
+		audioPlayer.transform.parent = null;
+		Destroy(audioPlayer, audioSource.clip.length);
+		Destroy(gameObject);
 	}
 }
