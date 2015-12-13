0edc84ba7c2a6b60c524ebba1877d75836edd1af
StaNov <stanovec@gmail.com>
2015-12-13 21:52:10 +0100

    grow scenes music

---
+++ Ludum Dare 34/Ludum Dare 34/Assets/cutscenes/growingBeard/GrowCutscenePlayer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrowCutscenePlayer : MonoBehaviour {
+
+	void Awake () {
+		Animator animator = GameObject.Find("cutscenePlayer").GetComponent<Animator>();
+		AudioSource audioSource = GetComponent<AudioSource>();
+		animator.speed = 1f / audioSource.clip.length;
+	}
+}
