96e8da8e24d252ca98deefc545f87c1156fea6c7
StaNov <stanovec@gmail.com>
2015-12-13 23:06:20 +0100

    cutscenes

---
+++ Ludum Dare 34/Ludum Dare 34/Assets/cutscenes/CutscenePlayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutscenePlayer : MonoBehaviour {
+
+	void Awake () {
+		AudioSource audioPlayer = GetComponent<AudioSource>();
+		Animator cameraAnimator = GameObject.FindObjectOfType<Animator>();
+
+		cameraAnimator.speed = 1f / audioPlayer.clip.length;
+	}
+}
