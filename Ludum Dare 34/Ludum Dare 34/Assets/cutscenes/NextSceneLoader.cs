5b0d248ce2a3fee3a784cbde62af5ef1d701073a
StaNov <stanovec@gmail.com>
2015-12-13 13:57:25 +0100

    intro cutscene

---
+++ Ludum Dare 34/Ludum Dare 34/Assets/cutscenes/NextSceneLoader.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class NextSceneLoader : MonoBehaviour {
+
+	public void LoadNextScene() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
