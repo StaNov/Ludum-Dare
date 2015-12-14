22dd26411429beab3aff14ef7e3684299a14a7df
StaNov <stanovec@gmail.com>
2015-12-14 02:27:17 +0100

    credits, escape exiter

---
+++ Ludum Dare 34/Ludum Dare 34/Assets/game/scripts/EscapeExiter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class EscapeExiter : MonoBehaviour {
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			if (SceneManager.GetActiveScene().buildIndex == 0) {
+				Application.Quit();
+			} else {
+				SceneManager.LoadScene(0);
+			}
+		}
+	}
+}
