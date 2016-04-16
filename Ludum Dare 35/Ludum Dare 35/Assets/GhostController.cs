5348a40fabebb78b45520e76c38d5da7e38a9a56
Stanislav Novak <stanislav.novak@master.cz>
2016-04-16 09:47:27 +0200

    shiftable

--- Ludum Dare 35/Ludum Dare 35/Assets/GhostController.cs
+++ Ludum Dare 35/Ludum Dare 35/Assets/GhostController.cs
@@ -6,6 +6,7 @@ public class GhostController : MonoBehaviour {
 	public float ghostSpeed = 3;
 
 	private Rigidbody2D rb;
+	private ShapeShiftable currentCollidingShiftable;
 
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
@@ -13,5 +14,19 @@ public class GhostController : MonoBehaviour {
 
 	void Update () {
 		rb.AddForce (new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * ghostSpeed);
+	}
+
+	void OnTriggerEnter2D (Collider2D col) {
+		ShapeShiftable shapeShiftable = col.GetComponent<ShapeShiftable> ();
+
+		if (shapeShiftable == null) {
+			return;
+		}
+
+		currentCollidingShiftable = shapeShiftable;
+	}
+
+	void OnTriggerExit2D (Collider2D col) {
+		currentCollidingShiftable = null;
 	}
 }
