9f622425c30e5b62e13878c2818df5ab9483f2ff
Stanislav Novak <stanislav.novak@master.cz>
2016-04-16 09:20:55 +0200

    ghost movement :)

---
+++ Ludum Dare 35/Ludum Dare 35/Assets/GhostController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostController : MonoBehaviour {
+
+	public float ghostSpeed = 3;
+
+	private Rigidbody2D rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+		rb.AddForce (new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * ghostSpeed);
+	}
+}
