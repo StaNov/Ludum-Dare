02d21429c0b577dd814dd9f97c453398e2415018
Stanislav Novak <stanislav.novak@master.cz>
2016-04-16 11:43:12 +0200

    end location

---
+++ Ludum Dare 35/Ludum Dare 35/Assets/Scripts/EndLocation.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class EndLocation : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col) {
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
