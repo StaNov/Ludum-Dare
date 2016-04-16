f0a102459cc4969288c6e02abacf54b21d85f860
Stanislav Novak <stanislav.novak@master.cz>
2016-04-16 17:01:07 +0200

    dialogs

---
+++ Ludum Dare 35/Ludum Dare 35/Assets/Scripts/Dialog.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class DialogLine {
+	[Multiline]
+	public string line;
+	public DialogActor actor;
+}
+
+public enum DialogActor {
+	ACTOR_1, ACTOR2
+}
+
+public class Dialog : MonoBehaviour {
+
+	public Color color1;
+	public Color color2;
+	public DialogLine[] dialogLines;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
