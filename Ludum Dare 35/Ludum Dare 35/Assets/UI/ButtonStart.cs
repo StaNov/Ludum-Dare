2f29dd004c4704751a675d33b5fbcb56ecb40281
Stanislav Novak <stanislav.novak@master.cz>
2016-04-16 16:15:47 +0200

    menu

---
+++ Ludum Dare 35/Ludum Dare 35/Assets/UI/ButtonStart.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ButtonStart : MonoBehaviour {
+
+	public void StartGame() {
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
