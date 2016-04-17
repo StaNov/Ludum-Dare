b8dda54d677745385c3c894a25d2cb4d9c54b91f
Stanislav Novak <stanislav.novak@master.cz>
2016-04-17 19:24:37 +0200

    credits

---
+++ Ludum Dare 35/Ludum Dare 35/Assets/UI/Scripts/Credits.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+
+	private AudioSource audioSource;
+	private Animator animator;
+
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+		animator = GetComponent<Animator> ();
+
+		animator.speed /= audioSource.clip.length;
+	}
+}
