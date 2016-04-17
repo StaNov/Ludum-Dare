96592ab97c12ee709ebd8cb2a6f7c8630e9ea08e
Stanislav Novak <stanislav.novak@master.cz>
2016-04-17 18:25:12 +0200

    rock fall

---
+++ Ludum Dare 35/Ludum Dare 35/Assets/Audio/Scripts/Cutscene.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cutscene : MonoBehaviour {
+	
+	void Start () {
+		Fader.instance.FadeOutAndPlayNextScene ();
+	}
+}
--- Ludum Dare 35/Ludum Dare 35/Assets/Audio/Scripts/EffectsPlayer.cs
+++ Ludum Dare 35/Ludum Dare 35/Assets/Audio/Scripts/EffectsPlayer.cs
@@ -7,6 +7,7 @@ public class EffectsPlayer : MonoBehaviour {
 	public AudioClip shapeshift;
 	public AudioClip openDoor;
 	public AudioClip endLevel;
+	public AudioClip rockFall;
 
 	private AudioSource source;
 	public static EffectsPlayer instance;
