863662e13a5367f72fedb471b18c1971bdb6accb
Stanislav Novak <stanislav.novak@master.cz>
2016-04-17 20:22:42 +0200

    fixes, back to menu from credits

---
+++ Ludum Dare 35/Ludum Dare 35/Assets/UI/Scripts/MenuSwitcher.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuSwitcher : MonoBehaviour {
+
+	public void SwitchToMenu () {
+		SceneManager.LoadScene (0);
+	}
+}
