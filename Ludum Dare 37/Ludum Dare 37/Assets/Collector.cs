8d8702b90b06008c824b1c82edbd2e3d346af282
StaNov <stanovec@gmail.com>
2016-12-10 12:30:46 +0100

    brani a pokladani veci

--- Ludum Dare 37/Ludum Dare 37/Assets/Collector.cs
+++ Ludum Dare 37/Ludum Dare 37/Assets/Collector.cs
@@ -1,15 +1,27 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Collector : MonoBehaviour {
 
 	public Transform slot;
-	
-	void OnTriggerEnter2D (Collider2D col) {
-		col.transform.SetParent(slot);
+	public Transform current = null;
 
-		col.transform.localPosition = Vector3.zero;
-		col.transform.localRotation = Quaternion.identity;
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.CompareTag(Tags.COLLECTIBLE) && current == null && (col.transform.parent == null || ! col.transform.parent.CompareTag(Tags.DESTINATION))) {
+			current = col.transform;
+
+			current.SetParent(slot);
+			current.localPosition = Vector3.zero;
+			current.localRotation = Quaternion.identity;
+		}
+
+		if (col.CompareTag(Tags.DESTINATION) && current != null) {
+			current.parent = col.transform;
+			current.localPosition = Vector3.zero;
+			current.localRotation = Quaternion.identity;
+
+			current = null;
+		}
 	}
 }
---
+++ Ludum Dare 37/Ludum Dare 37/Assets/Tags.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Tags {
+
+	public const string COLLECTIBLE = "Collectible";
+	public const string DESTINATION = "Destination";
+}
