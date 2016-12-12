10348f5a5f17ff2f4fd1b0ac5f90779a39e638f2
StaNov <stanovec@gmail.com>
2016-12-12 02:17:59 +0100

    v nějakým stavu, nevim

--- Ludum Dare 37/Ludum Dare 37/Assets/Scripts/GameOverHandler.cs
+++ Ludum Dare 37/Ludum Dare 37/Assets/Scripts/GameOverHandler.cs
@@ -1,21 +1,66 @@
+using System.Collections;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class GameOverHandler : MonoBehaviour {
-	
+
+	public IntroItem[] items;
+	public Camera camera;
+	public PernicekController PernicekCtrl;
+
+	public Transform finalPosition;
+	public float finalSize;
+	public RectTransform credits;
+	public Button playAgainButton;
+
+
+	private AudioSource audioSource;
 	private GameObject[] m_Destinations;
+	private bool ending = false;
 
 	void Awake() {
 		m_Destinations = GameObject.FindGameObjectsWithTag(Tags.DESTINATION);
+		audioSource = GetComponent<AudioSource>();
 	}
 	
 	void Update () {
+		if (ending) {
+			return;
+		}
+
 		foreach (GameObject dest in m_Destinations) {
 			if (dest.transform.childCount == 0) {
 				return;
 			}
 		}
 
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		PernicekCtrl.TurnedOn = false;
+		StartCoroutine(Ending());
+		ending = true;
+	}
+
+	private IEnumerator Ending() {
+
+		foreach (IntroItem item in items) {
+			audioSource.clip = item.clip;
+			audioSource.Play();
+			transform.DOMove(item.cameraEnd.position, item.clip.length).SetEase(Ease.InOutSine);
+			camera.DOOrthoSize(item.cameraSize, item.clip.length).SetEase(Ease.InOutSine);
+			Subtitles.ShowText(item.subtitle);
+			yield return new WaitForSeconds(item.clip.length);
+		}
+
+
+		Subtitles.HideText();
+		camera.DOOrthoSize(finalSize, 1);
+		camera.transform.DOMove(finalPosition.position, 1);
+
+		yield return new WaitForSeconds(1);
+
+		credits.DOMoveY(1000, 30);
+		yield return new WaitForSeconds(30);
+
+		playAgainButton.gameObject.SetActive(true);
 	}
 }
---
+++ Ludum Dare 37/Ludum Dare 37/Assets/Scripts/PlayAgain.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayAgain : MonoBehaviour {
+	
+	public void PlayAgainBKBJK () {
+		SceneManager.LoadScene(0);
+	}
+}
