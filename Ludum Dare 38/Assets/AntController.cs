c02126b305a0c5ba594d507afa95fa6421ec01ee
StaNov <stanovec@gmail.com>
2017-04-22 11:06:42 +0200

    mravenci pohyb pomoci RB a collideru

--- Ludum Dare 38/Assets/AntController.cs
+++ Ludum Dare 38/Assets/AntController.cs
@@ -6,6 +6,9 @@ public class AntController : MonoBehaviour {
 
 	public float moveSpeed = 1;
 	public float rotateSpeed = 1;
+	public AntController followingAnt = null;
+
+	private const float minimumDistance = 5;
 
 	private Rigidbody2D rb;
 	
@@ -13,11 +16,35 @@ public class AntController : MonoBehaviour {
 		rb = GetComponent<Rigidbody2D>();
 	}
 	
-	void Update () {
+	void FixedUpdate () {
+		if (followingAnt == null) {
+			ControlLeader();
+		} else {
+			ControlFollower();
+		}
+	}
+
+	private void ControlLeader() {
 		if (Input.GetAxisRaw("Vertical") > float.Epsilon) {
 			rb.MovePosition(transform.position + transform.up * moveSpeed * Time.deltaTime);
 		}
 
-		transform.Rotate(new Vector3(0, 0, - Input.GetAxisRaw("Horizontal") * rotateSpeed * 30 * Time.deltaTime));
+		transform.Rotate(new Vector3(0, 0, -Input.GetAxisRaw("Horizontal") * rotateSpeed * 30 * Time.deltaTime));
+	}
+
+	private void ControlFollower() {
+
+		var lookPos = followingAnt.transform.position - transform.position;
+		var newRotation = Quaternion.LookRotation(lookPos, Vector3.back);
+		newRotation.y = 0;
+		newRotation.x = 0;
+		transform.rotation = newRotation;
+
+		if (Vector3.Distance(followingAnt.transform.position, transform.position) < minimumDistance) {
+			return;
+		}
+		
+
+		rb.MovePosition(transform.position + transform.up * moveSpeed * Time.deltaTime);
 	}
 }
