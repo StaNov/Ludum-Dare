648e93e9bed3aed25bca5dc7c32bd1dfc8385874
StaNov <stanovec@gmail.com>
2017-04-22 21:48:21 +0200

    mravenci positionioning done

--- Ludum Dare 38/Assets/Collectible.cs
+++ Ludum Dare 38/Assets/Collectible.cs
@@ -40,17 +40,33 @@ public class Collectible : MonoBehaviour {
 		{
 			return;
 		}
+
+		if (! collision.collider.GetComponent<AntController>().isLeader)
+		{
+			return;
+		}
 		
 		foreach (AntController ant in AntsManager.GetActiveAnts())
 		{
 			ant.enabled = false;
-			ant.GetComponent<Collider2D>().enabled = false;
 			ant.transform.parent = transform;
-			ant.transform.localPosition = Vector3.zero;
-			ant.transform.localRotation = Quaternion.identity;
+			SetAntPositionAndRotationAfterHit(ant);
+			ant.GetComponent<Collider2D>().enabled = false;
 		}
 		
 		waitingForAnts = false;
+	}
+
+	private void SetAntPositionAndRotationAfterHit(AntController ant)
+	{
+		float xPos = Random.Range(-10f, 10f);
+		float yPos = Random.Range(-10f, 10f);
+		Vector3 position = new Vector3(xPos, yPos, 0);
+		position.Normalize();
+		position *= (GetComponent<CircleCollider2D>().radius + (ant.GetComponent<BoxCollider2D>().size.y / 2) / transform.localScale.x);
+
+		ant.transform.localPosition = position;
+		ant.transform.localRotation = Quaternion.LookRotation(Vector3.forward, Vector3.zero - ant.transform.localPosition);
 	}
 }
 
