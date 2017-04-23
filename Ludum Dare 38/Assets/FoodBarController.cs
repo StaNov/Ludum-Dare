965b900a021cd7674844d46e2260f5e9da75994e
StaNov <stanovec@gmail.com>
2017-04-23 23:11:28 +0200

    food bar

---
+++ Ludum Dare 38/Assets/FoodBarController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class FoodBarController : MonoBehaviour {
+
+	public AntHill antHill;
+	public RectTransform bar;
+
+	private float fullWidth;
+
+	private Text text;
+
+	
+	void Awake () {
+		text = GetComponentInChildren<Text>();
+		fullWidth = bar.sizeDelta.x;
+	}
+	
+	void Update () {
+		text.text = "Food: " + antHill.currentFoodSupply.ToString("0") + "/" + antHill.maxFoodSupply;
+		Vector2 size = bar.sizeDelta;
+		size.x = (antHill.currentFoodSupply / antHill.maxFoodSupply) * fullWidth;
+		//bar.sizeDelta = size;
+		bar.DOSizeDelta(size, 1f);
+	}
+}
