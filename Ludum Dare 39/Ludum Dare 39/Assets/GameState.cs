fe761362d9762bdb3d0b9ba8f0b9d0ad8b5c1eec
StaNov <stanovec@gmail.com>
2017-07-29 17:25:23 +0200

    sleep action, yaaaaaay :)

--- Ludum Dare 39/Ludum Dare 39/Assets/GameState.cs
+++ Ludum Dare 39/Ludum Dare 39/Assets/GameState.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,19 +15,44 @@ public class GameState : MonoBehaviour {
     public float FamilyHealth = 100;
     public float Age = 25;
 
+	public CurrentAction CurrentPlayerAction;
+	public CurrentAction CurrentPartnerAction;
+
 	public GameplayConstants Constants;
 
 	public static GameState Instance;
 	
 	private static float DeltaTimeInMinutes { get { return Time.deltaTime * (1.0f / 60.0f); }}
+	private float DeltaTimeByDurationPlayer { get { return Time.deltaTime * (1.0f / CurrentPlayerAction.Action.DurationInSeconds); }}
+	private float DeltaTimeByDurationPartner { get { return Time.deltaTime * (1.0f / CurrentPartnerAction.Action.DurationInSeconds); }}
 
 	private void Awake ()
 	{
 		Instance = this;
+		CurrentPlayerAction = null;
 	}
 	
-	private void Update ()
+	private void FixedUpdate ()
 	{
+		if (CurrentPlayerAction != null && CurrentPlayerAction.Action.Type != PlayerActionType.None)
+		{
+			MyEnergy += CurrentPlayerAction.Action.Effect.MyEnergy * DeltaTimeByDurationPlayer;
+			MyMaxEnergy += CurrentPlayerAction.Action.Effect.MyMaxEnergy * DeltaTimeInMinutes;
+			MyFood += CurrentPlayerAction.Action.Effect.MyFood * DeltaTimeInMinutes;
+			MyHappiness += CurrentPlayerAction.Action.Effect.MyHappiness * DeltaTimeInMinutes;
+			MyHealth += CurrentPlayerAction.Action.Effect.MyHealth * DeltaTimeInMinutes;
+			FamilyFood += CurrentPlayerAction.Action.Effect.FamilyFood * DeltaTimeInMinutes;
+			FamilyHappiness += CurrentPlayerAction.Action.Effect.FamilyHappiness * DeltaTimeInMinutes;
+			FamilyHealth += CurrentPlayerAction.Action.Effect.FamilyHealth * DeltaTimeInMinutes;
+
+			CurrentPlayerAction.RemainingTime -= Time.deltaTime;
+
+			if (CurrentPlayerAction.RemainingTime < 0)
+			{
+				CurrentPlayerAction = null;
+			}
+		}
+
 		MyEnergy -= Constants.ChangePerMinute.MyEnergy * DeltaTimeInMinutes;
 		MyMaxEnergy -= Constants.ChangePerMinute.MyMaxEnergy * DeltaTimeInMinutes;
 		MyFood -= Constants.ChangePerMinute.MyFood * DeltaTimeInMinutes;
@@ -36,5 +62,23 @@ public class GameState : MonoBehaviour {
 		FamilyHappiness -= Constants.ChangePerMinute.FamilyHappiness * DeltaTimeInMinutes;
 		FamilyHealth -= Constants.ChangePerMinute.FamilyHealth * DeltaTimeInMinutes;
 		Age += Constants.ChangePerMinute.Age * DeltaTimeInMinutes;
+	}
+
+	public void RunPlayerAction(string type)
+	{
+		PlayerAction action = Constants.GetPlayerAction((PlayerActionType) Enum.Parse(typeof(PlayerActionType), type));
+		
+		CurrentPlayerAction = new CurrentAction
+		{
+			Action = action,
+			RemainingTime = action.DurationInSeconds
+		};
+	}
+
+	[Serializable]
+	public class CurrentAction
+	{
+		public PlayerAction Action;
+		public float RemainingTime;
 	}
 }
--- Ludum Dare 39/Ludum Dare 39/Assets/GameplayConstants.cs
+++ Ludum Dare 39/Ludum Dare 39/Assets/GameplayConstants.cs
@@ -4,18 +4,66 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "GameplayConstants", menuName = "Create GameplayConstants", order = 1)]
 public class GameplayConstants : ScriptableObject
 {
-	public ChangePerMinuteClass ChangePerMinute;
-	
-	[Serializable]
-	public class ChangePerMinuteClass {
-		public float MyEnergy = 10;
-		public float MyMaxEnergy = 1;
-		public float MyFood = 5;
-		public float MyHappiness = 5;
-		public float MyHealth = 5;
-		public float FamilyFood = 5;
-		public float FamilyHappiness = 5;
-		public float FamilyHealth = 5;
-		public float Age = 2;
+	public StatsDifference ChangePerMinute = new StatsDifference
+	{
+		MyEnergy = 10,
+		MyMaxEnergy = 1,
+		MyFood = 5,
+		MyHappiness = 5,
+		MyHealth = 5,
+		FamilyFood = 5,
+		FamilyHappiness = 5,
+		FamilyHealth = 5,
+		Age = 2
+	};
+
+	public PlayerAction[] PlayerActions = {
+		new PlayerAction
+		{
+			Type = PlayerActionType.Sleep,
+			DurationInSeconds = 10,
+			Effect = new StatsDifference
+			{
+				MyEnergy = 10
+			}
+		}
+	};
+
+	public PlayerAction GetPlayerAction(PlayerActionType type)
+	{
+		foreach (var action in PlayerActions)
+		{
+			if (type == action.Type)
+				return action;
+		}
+
+		throw new Exception("NO ACTION FOUND!");
 	}
+}
+	
+[Serializable]
+public class StatsDifference
+{
+	public float MyEnergy;
+	public float MyMaxEnergy;
+	public float MyFood;
+	public float MyHappiness;
+	public float MyHealth;
+	public float FamilyFood;
+	public float FamilyHappiness;
+	public float FamilyHealth;
+	public float Age;
+}
+
+[Serializable]
+public class PlayerAction
+{
+	public PlayerActionType Type;
+	public float DurationInSeconds;
+	public StatsDifference Effect;
+}
+
+public enum PlayerActionType
+{
+	None, Sleep
 }
