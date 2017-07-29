40dd8d1c95efb80a80a438290a14ab6352990e73
StaNov <stanovec@gmail.com>
2017-07-29 14:56:57 +0200

    základní zobrazování potřeb

---
+++ Ludum Dare 39/Ludum Dare 39/Assets/GameState.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour {
+
+    public float MyEnergy = 100;
+    public float MyMaxEnergy = 100;
+    public float MyFood = 100;
+    public float MyHappiness = 100;
+    public float MyHealth = 100;
+    public float FamilyFood = 100;
+    public float FamilyHappiness = 100;
+    public float FamilyHealth = 100;
+
+	public static GameState Instance;
+
+	private void Awake ()
+	{
+		Instance = this;
+	}
+	
+	private void Update () {
+		
+	}
+}
---
+++ Ludum Dare 39/Ludum Dare 39/Assets/NeedsUpdater.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NeedsUpdater : MonoBehaviour
+{
+
+	public Text MyEnergy;
+	public Text MyFood;
+	public Text MyHappiness;
+	public Text MyHealth;
+	public Text FamilyFood;
+	public Text FamilyHappiness;
+	public Text FamilyHealth;
+
+	private void Update ()
+	{
+		MyEnergy.text = "My energy: " + GameState.Instance.MyEnergy + " / " + GameState.Instance.MyMaxEnergy;
+		MyFood.text = "My food: " + GameState.Instance.MyFood;
+		MyHappiness.text = "My happiness: " + GameState.Instance.MyHappiness;
+		MyHealth.text = "My health: " + GameState.Instance.MyHealth;
+		FamilyFood.text = "Family food: " + GameState.Instance.FamilyFood;
+		FamilyHappiness.text = "Family happiness: " + GameState.Instance.FamilyHappiness;
+		FamilyHealth.text = "Family health: " + GameState.Instance.FamilyHealth;
+	}
+}
