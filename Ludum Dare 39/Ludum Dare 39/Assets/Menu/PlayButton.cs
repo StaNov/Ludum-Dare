8ddfbd5d65e6dfa8fe077e00a64cb7088d66f24a
StaNov <stanovec@gmail.com>
2017-07-30 23:31:42 +0200

    menu

---
+++ Ludum Dare 39/Ludum Dare 39/Assets/Menu/PlayButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour {
+
+	public void Play () {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+	
+}
