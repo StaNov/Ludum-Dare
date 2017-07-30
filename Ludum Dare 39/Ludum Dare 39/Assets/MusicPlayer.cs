78df9f51e7ff85418d2d398e7c40942e80da2154
StaNov <stanovec@gmail.com>
2017-07-30 22:26:39 +0200

    musics

---
+++ Ludum Dare 39/Ludum Dare 39/Assets/MusicPlayer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+	public AudioSource Source;
+	public AudioClip MainTune;
+	public AudioClip StressTune;
+	public AudioClip EndTune;
+	public GameState State;
+
+	void Start ()
+	{
+		Source.clip = MainTune;
+		Source.Play();
+	}
+	
+	void Update ()
+	{
+		if (State.GameOver != GameOverReason.StillPlaying)
+		{
+			Source.clip = EndTune;
+			Source.Play();
+			Source.loop = false;
+			enabled = false;
+			return;
+		}
+
+		if (State.MyEnergy < 15 && Source.clip == MainTune)
+		{
+			Source.clip = StressTune;
+			Source.Play();
+		}
+
+		if (State.MyEnergy > 20 && Source.clip == StressTune)
+		{
+			Source.clip = MainTune;
+			Source.Play();
+		}
+	}
+}
