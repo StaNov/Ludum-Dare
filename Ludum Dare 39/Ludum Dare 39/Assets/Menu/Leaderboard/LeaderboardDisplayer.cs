bce66620a0559889e4e88b171e4cf62f5f99a3bd
StaNov <stanovec@gmail.com>
2017-08-06 21:44:56 +0200

    reusable manager

--- Ludum Dare 39/Ludum Dare 39/Assets/Menu/Leaderboard/LeaderboardDisplayer.cs
+++ Ludum Dare 39/Ludum Dare 39/Assets/Menu/Leaderboard/LeaderboardDisplayer.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEditor;
+﻿using System.Collections;
 using UnityEngine;
 
 public class LeaderboardDisplayer : MonoBehaviour
@@ -17,64 +12,16 @@ public class LeaderboardDisplayer : MonoBehaviour
 			Destroy(child.gameObject);
 		}
 		
-		WWW www = new WWW("http://dreamlo.com/lb/59875abbb0b05d1ad4be0123/pipe/10");
-
-		while (! www.isDone)
-			yield return null;
-
-		List<Line> lines = ParseLines(www.text.Trim());
-
-		if (! string.IsNullOrEmpty(PlayerNameManager.PlayerName) && ! lines.Any(line => line.PlayerName.Equals(PlayerNameManager.PlayerName)))
-		{
-			www = new WWW("http://dreamlo.com/lb/59875abbb0b05d1ad4be0123/pipe-get/" + WWW.EscapeURL(PlayerNameManager.PlayerName));
-
-			while (!www.isDone)
-				yield return null;
-			
-			lines.AddRange(ParseLines(www.text.Trim()));
-		}
-			
-		InstantiateLeaderboardLines(lines);
+		yield return ReusableLeaderboardManager.GetLeaderboard(
+			"59875abbb0b05d1ad4be0123", 
+			PlayerNameManager.PlayerName,
+			InstantiateLine);
 	}
 
-	private void InstantiateLeaderboardLines(List<Line> lines)
+	private void InstantiateLine(int place, string playerName, int score)
 	{
-		foreach (Line line in lines)
-		{
-			var leaderBoardLine = Instantiate(LinePrefab).GetComponent<LeaderBoardLine>();
-			leaderBoardLine.SetValues(line.Place, line.PlayerName, line.Score);
-			leaderBoardLine.transform.SetParent(transform, false);
-		}
-	}
-
-	private List<Line> ParseLines(string text)
-	{
-		if (string.IsNullOrEmpty(text))
-			return new List<Line>();
-		
-		string[] linesStrings = text.Split('\n');
-		
-		List<Line> result = new List<Line>();
-
-		foreach (string line in linesStrings)
-		{
-			string[] fields = line.Split('|');
-			Line newLine = new Line
-			{
-				Place = int.Parse(fields[5]) + 1,
-				PlayerName = fields[0],
-				Score = int.Parse(fields[1])
-			};
-			result.Add(newLine);
-		}
-
-		return result;
-	}
-
-	private class Line
-	{
-		public int Place;
-		public string PlayerName;
-		public int Score;
+		var leaderBoardLine = Instantiate(LinePrefab).GetComponent<LeaderBoardLine>();
+		leaderBoardLine.SetValues(place, playerName, score);
+		leaderBoardLine.transform.SetParent(transform, false);
 	}
 }
---
+++ Ludum Dare 39/Ludum Dare 39/Assets/ReusableLeaderboardManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class ReusableLeaderboardManager {
+
+	public static IEnumerator GetLeaderboard(
+		string leaderboardId, 
+		string currentPlayerName, 
+		Action<int, string, int> forEachLine)
+	{
+		WWW www = new WWW("http://dreamlo.com/lb/" + leaderboardId + "/pipe/10");
+
+		while (! www.isDone)
+			yield return null;
+
+		List<Line> lines = ParseLines(www.text.Trim());
+
+		if (! string.IsNullOrEmpty(currentPlayerName) && ! lines.Any(line => line.PlayerName.Equals(currentPlayerName)))
+		{
+			www = new WWW("http://dreamlo.com/lb/" + leaderboardId + "/pipe-get/" + WWW.EscapeURL(currentPlayerName));
+
+			while (!www.isDone)
+				yield return null;
+			
+			lines.AddRange(ParseLines(www.text.Trim()));
+		}
+			
+		foreach (Line line in lines)
+		{
+			forEachLine(line.Place, line.PlayerName, line.Score);
+		}
+	}
+
+	private static List<Line> ParseLines(string text)
+	{
+		if (string.IsNullOrEmpty(text))
+			return new List<Line>();
+		
+		string[] linesStrings = text.Split('\n');
+		
+		List<Line> result = new List<Line>();
+
+		foreach (string line in linesStrings)
+		{
+			string[] fields = line.Split('|');
+			Line newLine = new Line
+			{
+				Place = int.Parse(fields[5]) + 1,
+				PlayerName = fields[0],
+				Score = int.Parse(fields[1])
+			};
+			result.Add(newLine);
+		}
+
+		return result;
+	}
+
+	private class Line
+	{
+		public int Place;
+		public string PlayerName;
+		public int Score;
+	}
+}
