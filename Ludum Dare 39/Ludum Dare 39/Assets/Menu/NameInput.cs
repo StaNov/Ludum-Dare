e8ebc256146e34d5ba9d53ebc84a59ba80417932
StaNov <stanovec@gmail.com>
2017-08-06 22:41:41 +0200

    name input

---
+++ Ludum Dare 39/Ludum Dare 39/Assets/Menu/NameInput.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NameInput : MonoBehaviour
+{
+	public InputField Input;
+	
+	void Start ()
+	{
+		Input.text = PlayerNameManager.PlayerName;
+	}
+	
+	public void OnEditEnd () {
+		PlayerNameManager.PlayerName = Input.text;
+	}
+}
