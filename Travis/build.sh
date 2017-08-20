5fc233f4cf4a099085c24b99a32fff1519736375
StaNov <stanovec@gmail.com>
2017-08-20 18:43:58 +0200

    single line commands

--- Travis/build.sh
+++ Travis/build.sh
@@ -3,24 +3,10 @@
 project="GameOfLife"
 
 echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity 
-  -batchmode 
-  -nographics 
-  -silent-crashes 
-  -logFile $(pwd)/unity.log 
-  -projectPath "$(pwd)/Ludum Dare 39/Ludum Dare 39"
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" 
-  -quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $(pwd)/unity.log -projectPath "$(pwd)/Ludum Dare 39/Ludum Dare 39" -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" -quit
 
 echo "Attempting to build $project for WebGL"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity 
-  -batchmode 
-  -nographics 
-  -silent-crashes 
-  -logFile $(pwd)/unity.log 
-  -projectPath "$(pwd)/Ludum Dare 39/Ludum Dare 39"
-  -buildWebGLPlayer "$(pwd)/Build/webgl" 
-  -quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $(pwd)/unity.log -projectPath "$(pwd)/Ludum Dare 39/Ludum Dare 39" -buildWebGLPlayer "$(pwd)/Build/webgl" -quit
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
--- Travis/test.sh
+++ Travis/test.sh
@@ -3,14 +3,7 @@
 project="GameOfLife"
 
 echo "Attempting to test $project in Editor."
-/Applications/Unity/Unity.app/Contents/MacOS/Unity
-  -runTests
-  -batchmode 
-  -nographics 
-  -silent-crashes 
-  -logFile $(pwd)/unity.log 
-  -projectPath "$(pwd)/Ludum Dare 39/Ludum Dare 39"
-  -quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -runTests -batchmode -nographics -silent-crashes -logFile $(pwd)/unity.log -projectPath "$(pwd)/Ludum Dare 39/Ludum Dare 39" -quit
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
