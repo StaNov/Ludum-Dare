c6b9a709d88a38bd9749f7a6cad54516efb373ad
StaNov <stanovec@gmail.com>
2017-08-20 19:02:36 +0200

    travis stuff

--- Travis/build.sh
+++ Travis/build.sh
@@ -1,17 +1,26 @@
 #! /bin/sh
 
-project="GameOfLife"
+project="MyLudumDareGame"
 
 echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $(pwd)/unity.log -projectPath "$(pwd)/Ludum Dare 39/Ludum Dare 39" -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" -quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $PROJECT_DIR \
+  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+  -quit
 
 echo "Attempting to build $project for WebGL"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $(pwd)/unity.log -projectPath "$(pwd)/Ludum Dare 39/Ludum Dare 39" -buildWebGLPlayer "$(pwd)/Build/webgl" -quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $PROJECT_DIR \
+  -buildWebGLPlayer "$(pwd)/Build/webgl/" \
+  -quit
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
-
-
-echo 'Attempting to zip builds'
-zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
-zip -r $(pwd)/Build/webgl.zip $(pwd)/Build/webgl/
--- Travis/test.sh
+++ Travis/test.sh
@@ -3,7 +3,14 @@
 project="GameOfLife"
 
 echo "Attempting to test $project in Editor."
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -runTests -batchmode -nographics -silent-crashes -logFile $(pwd)/unity.log -projectPath "$(pwd)/Ludum Dare 39/Ludum Dare 39" -quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -runTests \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $PROJECT_DIR \
+  -quit
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
