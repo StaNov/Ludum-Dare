103f6724711328c769722c7d6c4d4a71191126d5
StaNov <stanovec@gmail.com>
2017-08-20 17:12:34 +0200

    travis CI added

---
+++ Travis/build.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+
+project="GameOfLife"
+
+echo "Attempting to build $project for Windows"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity 
+  -batchmode 
+  -nographics 
+  -silent-crashes 
+  -logFile $(pwd)/unity.log 
+  -projectPath $(pwd) 
+  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" 
+  -quit
+
+echo "Attempting to build $project for WebGL"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity 
+  -batchmode 
+  -nographics 
+  -silent-crashes 
+  -logFile $(pwd)/unity.log 
+  -projectPath $(pwd) 
+  -buildWebGLPlayer "$(pwd)/Build/webgl" 
+  -quit
+
+echo 'Logs from build'
+cat $(pwd)/unity.log
+
+
+echo 'Attempting to zip builds'
+zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
+zip -r $(pwd)/Build/webgl.zip $(pwd)/Build/webgl/
