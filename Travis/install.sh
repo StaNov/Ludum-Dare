eed2da72f5fe065b27240ead1147b86946619779
StaNov <stanovec@gmail.com>
2017-08-20 20:27:16 +0200

    build removed, failing test removed

--- Travis/install.sh
+++ Travis/install.sh
@@ -21,5 +21,3 @@ install() {
 }
 
 install "MacEditorInstaller/Unity-$VERSION.pkg"
-#install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
-#install "MacEditorTargetInstaller/UnitySetup-WebGL-Support-for-Editor-$VERSION.pkg"
