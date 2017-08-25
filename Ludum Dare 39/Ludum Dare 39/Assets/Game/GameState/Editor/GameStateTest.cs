595cbb23774eb27afb1fa8ad808b8b2a94436829
StaNov <stanovec@gmail.com>
2017-08-25 22:16:36 +0200

    first test :)

---
+++ Ludum Dare 39/Ludum Dare 39/Assets/Game/GameState/Editor/GameStateTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System;
+
+public class GameStateTest
+{
+	private GameState State;
+
+	public void Setup()
+	{
+		State = new GameObject().AddComponent<GameState>();
+	}
+
+	[Test]
+	public void NewEditModeTestSimplePasses()
+	{
+		Setup();
+		Assert.NotNull(State);
+	}
+}
