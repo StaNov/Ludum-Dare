5a0ba7845f6a738c30665a1a11cb9c5da20e44c0
StaNov <stanovec@gmail.com>
2017-08-27 00:17:10 +0200

    more interesting tests :)

--- Ludum Dare 39/Ludum Dare 39/Assets/Game/GameState/Test/GameStateTest.cs
+++ Ludum Dare 39/Ludum Dare 39/Assets/Game/GameState/Test/GameStateTest.cs
@@ -8,6 +8,9 @@ public class GameStateTest
 {
 	private GameState State;
 
+	private const int InitialAge = 40;
+	private const int InitialMoney = 12345;
+
 	public IEnumerator Setup()
 	{
 		yield return DestroyAllObjectsInScene();
@@ -16,15 +19,24 @@ public class GameStateTest
 		State.Constants = ScriptableObject.CreateInstance<GameplayConstants>();
 		State.Constants.InitialValues = new StatsDifference
 		{
-			Age = 40,
-			Money = 12345
+			Age = InitialAge,
+			Money = InitialMoney,
+			MyMaxEnergy = 1111,
+			MyEnergy = 1000,
+			MyHappiness = 2,
+			MyHealth = 30,
+			FamilyFood = 10,
+			FamilyHappiness = 20,
+			FamilyHealth = 30,
+			FoodSupplies = 99,
+			MyFood = 20
 		};
 		State.Constants.ChangePerMinute = new StatsDifference
 		{
-			Age = 11
+			Age = 11111
 		};
 
-		yield return null;
+		yield return new WaitForFixedUpdate();
 	}
 
 	private static IEnumerator DestroyAllObjectsInScene()
@@ -38,10 +50,17 @@ public class GameStateTest
 	}
 
 	[UnityTest]
-	public IEnumerator StateIsInitializedAfterSceneLoad()
+	public IEnumerator MoneyStatIsInitializedAfterSceneLoad()
 	{
 		yield return Setup();
-		Assert.AreEqual(State.Age, 40, 0.01f);
-		Assert.AreEqual(State.Money, 12345);
+		Assert.AreEqual(State.Money, InitialMoney);
+	}
+
+	[UnityTest]
+	public IEnumerator AgeIsGreaterAfterCoupleOfFrames()
+	{
+		yield return Setup();
+		yield return new WaitForSeconds(0.5f);
+		Assert.Greater(State.Age, InitialAge + 1);
 	}
 }
