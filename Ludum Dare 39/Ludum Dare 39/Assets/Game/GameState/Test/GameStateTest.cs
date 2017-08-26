49bbfc604f1c9e00c30464876e39ff1661908a9f
StaNov <stanovec@gmail.com>
2017-08-26 22:05:51 +0200

    first play mode test

---
+++ Ludum Dare 39/Ludum Dare 39/Assets/Game/GameState/Test/GameStateTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System;
+using UnityEngine.SceneManagement;
+
+public class GameStateTest
+{
+	private GameState State;
+
+	public IEnumerator Setup()
+	{
+		SceneManager.LoadScene("GameStateTestScene", LoadSceneMode.Additive);
+		yield return null;
+		State = GameObject.Find("GameObject").GetComponent<GameState>();
+	}
+
+	[UnityTest]
+	public IEnumerator StateIsNotNullAfterTestSceneIsLoaded()
+	{
+		yield return Setup();
+		Assert.NotNull(State);
+	}
+}
