1212a08928c5df4e24dcc6a88a19ac8925059024
StaNov <stanovec@gmail.com>
2017-09-09 21:52:54 +0200

    logs to console directly

--- Travis/test.sh
+++ Travis/test.sh
@@ -5,21 +5,17 @@ project="GameOfLife"
 TEST_RESULTS_PATH="$(pwd)/Build/testResults.xml"
 TEST_PLATFORM=$1
 
-echo "\n\n===== Attempting to test project in Editor =====\n\n"
+echo "\n\n===== Running tests in $TEST_PLATFORM =====\n\n"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -runTests \
   -testPlatform $TEST_PLATFORM \
   -batchmode \
   -nographics \
   -silent-crashes \
-  -logFile $(pwd)/unity.log \
   -projectPath "$PROJECT_DIR" \
   -testResults "$TEST_RESULTS_PATH"
 
 EXIT_CODE=$?
-
-echo 'Logs from test'
-cat $(pwd)/unity.log
 
 echo '\n\n===== Test results if file exists =====\n\n'
 if [ -f "$TEST_RESULTS_PATH" ]; then
