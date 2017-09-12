5a5545b8459025892f1230f6e6c4871294940b2b
StaNov <stanovec@gmail.com>
2017-09-12 22:21:36 +0200

    state item float to separate file

---
+++ Ludum Dare 39/Ludum Dare 39/Assets/Game/GameState/StateItemFloat.cs
@@ -0,0 +1,76 @@
+using System;
+using UnityEngine;
+
+public class StateItemFloat : StateItem
+{
+	private float _value;
+	private float _minValue;
+	private Func<float> _maxValue;
+	private Func<StatsDifference, float> _getDifferenceValue;
+	private Func<bool> _shouldBeUpdated = () => true; // initial values can be initialized
+	private float _changePerMinute;
+
+	public StateItemFloat(float minValue, float maxValue, GameplayConstants constants, Func<StatsDifference, float> getDifferenceValue, Func<bool> shouldBeUpdated) :
+		this(minValue, () => maxValue, constants, getDifferenceValue, shouldBeUpdated)
+	{ }
+
+	// TODO create builder
+	// TODO redesign the arguments needed in constructor
+	// TODO pass gamestate to differenceHasZeroEffect or shouldBeUpdated?
+	public StateItemFloat(float minValue, Func<float> getMaxValue, GameplayConstants constants, Func<StatsDifference, float> getDifferenceValue, Func<bool> shouldBeUpdated)
+	{
+		_minValue = minValue;
+		_maxValue = getMaxValue;
+		_getDifferenceValue = getDifferenceValue;
+		_changePerMinute = _getDifferenceValue(constants.ChangePerMinute);
+		Value = _getDifferenceValue(constants.InitialValues);
+		_shouldBeUpdated = shouldBeUpdated;
+	}
+
+	public void ApplyDifferenceByTime(float deltaTime)
+	{
+		float deltaTimeInMinutes = deltaTime / 60;
+
+		Value += _changePerMinute * deltaTimeInMinutes;
+	}
+
+	public void ApplyDifference(StatsDifference difference, float multiplier = 1)
+	{
+		Value += _getDifferenceValue(difference) * multiplier;
+	}
+
+	public bool IsGameOverBecauseOfThis()
+	{
+		return _value <= 0;
+	}
+
+	public bool DifferenceHasZeroEffect(StatsDifference difference)
+	{
+		return _getDifferenceValue(difference) == 0;
+	}
+
+	public T GetValue<T>()
+	{
+		if (typeof(T) != typeof(float))
+		{
+			throw new Exception("You are trying to get value of type " + typeof(T) + ", but this StateItem is of type " + typeof(float) + "."); // float -> generic
+		}
+
+		return (T)Convert.ChangeType(Value, typeof(T));
+	}
+
+	public float Value
+	{
+		get
+		{
+			return _value;
+		}
+		private set
+		{
+			if (_shouldBeUpdated())
+			{
+				_value = Mathf.Clamp(value, _minValue, _maxValue());
+			}
+		}
+	}
+}
