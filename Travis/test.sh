c28ab74ff2724d920a7c3bf2b53b4a21556285e6
StaNov <stanovec@gmail.com>
2017-09-17 14:40:40 +0200

    tweaking of build script

--- Travis/test.sh
+++ Travis/test.sh
@@ -6,8 +6,9 @@ TEST_RESULTS_PATH="$(pwd)/Build/testResults.xml"
 TEST_PLATFORM=$1
 RUN_TESTS_FOLD_NAME="runTests_$TEST_PLATFORM"
 
-echo "travis_fold:start:$RUN_TESTS_FOLD_NAME"
-echo "\n\n===== Running tests in $TEST_PLATFORM =====\n\n"
+# collapsible logs: https://github.com/travis-ci/travis-ci/issues/2285
+echo -en "travis_fold:start:$RUN_TESTS_FOLD_NAME\r"
+echo "===== Running tests in $TEST_PLATFORM =====\n\n"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -runTests \
   -testPlatform $TEST_PLATFORM \
@@ -19,14 +20,13 @@ echo "\n\n===== Running tests in $TEST_PLATFORM =====\n\n"
   -testResults "$TEST_RESULTS_PATH"
 
 EXIT_CODE=$?
-echo "travis_fold:end:$RUN_TESTS_FOLD_NAME"
+echo -en "travis_fold:end:$RUN_TESTS_FOLD_NAME\r"
 
-echo '\n\n===== Test results if file exists =====\n\n'
 if [ -f "$TEST_RESULTS_PATH" ]; then
-  echo '\n\n===== Test results of $TEST_PLATFORM =====\n\n';
+  echo "===== Test results of $TEST_PLATFORM =====\n\n";
   cat "$TEST_RESULTS_PATH";
 else
-  echo '\n\n===== Test results of $TEST_PLATFORM do not exist! =====\n\n';
+  echo "\n\n===== Test results of $TEST_PLATFORM do not exist! =====\n\n";
 fi
 
 exit $EXIT_CODE
