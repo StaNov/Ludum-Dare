4354e1c9c8f17c13fd0b665ea216a8df35a5e2fa
StaNov <stanovec@gmail.com>
2017-09-17 14:21:22 +0200

    GameStateFactory created

---
+++ Ludum Dare 39/Ludum Dare 39/Assets/Game/GameState/GameStateFactory.cs
@@ -0,0 +1,12 @@
+namespace GameOfLife.GameState
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	public class GameStateFactory
+	{
+
+		
+	}
+}
---
+++ Ludum Dare 39/Ludum Dare 39/Assets/Game/GameState/Test/Editor/GameStateFactoryTest.cs
@@ -0,0 +1,12 @@
+namespace GameOfLife.GameState {
+	using NUnit.Framework;
+
+	public class GameStateFactoryTest {
+
+		[Test]
+		public void Test()
+		{
+			new GameStateFactory();
+		}
+	}
+}
