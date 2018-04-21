1bd6d11d81cbb90d5e210bd6cc38bb6ec4119bd7
StaNov <stanovec@gmail.com>
2018-04-21 22:39:21 +0200

    steps

--- Ludum Dare 41/main.py
+++ Ludum Dare 41/main.py
@@ -1,1 +1,50 @@
-print("Hello world!")
+print("""
+==================================================
+==================================================
+
+        The Pond: An Unexpected Journey
+        
+  
+  Genre: Walking Simulator + Terminal Based Game
+  
+
+  Game by StaNov
+
+  Created for Ludum Dare 41
+
+  Theme: Combine 2 Incompatible Genres
+  
+==================================================
+
+You stand by a big tree. It is a really beautiful one.
+
+On the horizon, you see an even more beautiful pond. You really feel like you want to swim a bit in it.
+
+It’s quite far, but you really want to touch the water. So you decided to go there.
+
+You can start your journey by typing “step”.
+""")
+
+user_input = None
+
+while user_input != "step" and user_input != "s":
+    user_input = input("> ")
+
+user_input = None
+
+print("""
+==================================================
+
+Congratulations! You took one step from the tree towards the pond.
+""")
+
+while user_input != "step" and user_input != "s":
+    user_input = input("> ")
+
+user_input = None
+
+print("""
+==================================================
+
+You took another step to the pond.
+""")
