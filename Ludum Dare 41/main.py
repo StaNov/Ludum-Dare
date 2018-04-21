b2b079566b68b372667965d7bd53867dfed63657
StaNov <stanovec@gmail.com>
2018-04-22 00:12:18 +0200

    added instructions

--- Ludum Dare 41/main.py
+++ Ludum Dare 41/main.py
@@ -1,3 +1,19 @@
 import game
 
+"""
+  _____ _   _  _____ _______ _____  _    _  _____ _______ _____ ____  _   _  _____ 
+ |_   _| \ | |/ ____|__   __|  __ \| |  | |/ ____|__   __|_   _/ __ \| \ | |/ ____|
+   | | |  \| | (___    | |  | |__) | |  | | |       | |    | || |  | |  \| | (___  
+   | | | . ` |\___ \   | |  |  _  /| |  | | |       | |    | || |  | | . ` |\___ \ 
+  _| |_| |\  |____) |  | |  | | \ \| |__| | |____   | |   _| || |__| | |\  |____) |
+ |_____|_| \_|_____/   |_|  |_|  \_\\____/ \_____|  |_|  |_____\____/|_| \_|_____/ 
+
+
+0. Don't browse the source code!
+1. Enlarge the console on the right enough to read its content comfortably.
+2. Click the Run button above this text.
+3. Make sure that the console is focused (click into it somewhere)
+4. Enjoy the game :)
+"""
+
 game.main()
