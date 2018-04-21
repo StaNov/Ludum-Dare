d65753e05aed81f771b2572830573e0b5ec526c2
StaNov <stanovec@gmail.com>
2018-04-21 23:56:47 +0200

    main refactored to game

---
+++ Ludum Dare 41/game.py
@@ -0,0 +1,23 @@
+import texts
+
+
+def main():
+    print(texts.intro_text)
+    input("Press Enter to continue...\n(if it doesn't work, make sure that this console is focused)")
+
+    for step_text in texts.step_texts:
+        print("""
+
+
+
+
+
+==================================================
+
+{}
+""".format(step_text))
+
+        user_input = None
+
+        while user_input != "step" and user_input != "s":
+            user_input = input("> ")
