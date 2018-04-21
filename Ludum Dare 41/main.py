f0e58b2238fe0e92c464217a85e907efd9aad5a3
StaNov <stanovec@gmail.com>
2018-04-21 23:21:00 +0200

    texts polishing

--- Ludum Dare 41/main.py
+++ Ludum Dare 41/main.py
@@ -1,17 +1,19 @@
 import texts
 
 print(texts.intro_text)
+input("Press Enter to continue...\n(if it doesn't work, make sure that this console is focused)")
 
 for step_text in texts.step_texts:
-    user_input = None
-
-    while user_input != "step" and user_input != "s":
-        user_input = input("> ")
-
     print("""
+
 
 
 ==================================================
 
 {}
 """.format(step_text))
+
+    user_input = None
+
+    while user_input != "step" and user_input != "s":
+        user_input = input("> ")
--- Ludum Dare 41/texts.py
+++ Ludum Dare 41/texts.py
@@ -8,7 +8,7 @@ intro_text = """
   Genre: Walking Simulator + Terminal Based Game
   
 
-  Game by StaNov
+  Game by StaNov  (www.StaNov.cz)
 
   Created for Ludum Dare 41
 
@@ -16,60 +16,128 @@ intro_text = """
   
 ==================================================
 
-You stand by a big tree. It is a really beautiful one.
-
-On the horizon, you see an even more beautiful pond. You really feel like you want to swim a bit in it.
-
-It’s quite far, but you really want to touch the water. So you decided to go there.
-
-You can start your journey by typing “step”.
 """
 
 step_texts = [
+    "You stand by a big tree. It is a really beautiful one.\n\n"
+    "On the horizon, you can see an even more beautiful pond.\n\n"
+    "You really feel like you want to swim a bit in it.\n\n"
+    "It’s quite far, but you really want to touch the water with your body.\n\n"
+    "So you decided to go there.\n\n"
+    "You can start your journey by typing 'step'.",
+
     "Congratulations! You took one step from the tree towards the pond.\n\nTry another one!",
+
     "You took another step to the pond.\n\nAh! The warm water is getting closer!\n\nGo on, you're doing great!",
+
     "And another step!",
+
     "And another one.",
+
     "...",
-    "Yea, it’s really quite far.\n\nAs I mentioned before.",
+
+    "Yea, it’s really quite far.\n\n"
+    "As I mentioned before.",
+
     "Left foot.",
+
     "Right foot.",
+
+    "And pressing 'arrow up' doesn't repeat the command, sorry...",
+
     "Left foot.",
+
     "Right foot.",
+
     "Left foot.",
-    "Aaaaaand… another left foot! Because of all the joy of walking, you jumped to the air!\n\nWeee!",
-    "Fun fact: There is an event in Brno where people walk around the city. One hundred kilometers during 24 hours.",
+
+    "Aaaaaand... another left foot!\n\n"
+    "Because of all the joy of walking, you jumped to the air!\n\n"
+    "Weee!",
+
+    "Right foot.",
+
+    "Fun fact: There is an event in Brno where people walk around the city.\n\n"
+    "One hundred kilometers during 24 hours.\n\n"
+    "Crazy people.",
+
     "Yea, I mentioned it because I live in Brno.",
-    "Brno is the greatest city in Czechia.",
+
+    "Brno is the biggest city in Czechia.",
+
     "...",
-    "Well, it’s not, but people from Prague are angry when we say it to foreigners who don’t already know.",
+
+    "Well, it’s not, but people from Prague get angry when we say it to foreigners who don’t already know.",
+
     "Ha ha.",
+
     "But foreigners haven’t even noticed that Czechoslovakia has split into two states.",
+
     "It was in 1993.",
+
     "There, that’s an interesting history thing you learnt today!",
+
     "By the way, Czechia is the state where Kingdom Come Deliverance is placed and developed.",
+
     "Also Factorio is developed by Czechians.",
+
+    "And Chameleon Run.",
+
     "And Shadowgun Legends.",
+
     "And Chuchel.",
+
     "And many others.",
-    "…",
+
+    "...",
+
     "So!",
-    "You slowly progress to the pond. But you don’t even mind that the journey is so long because you are so entertained by the narrative!",
+
+    "You slowly progress to the pond.\n\n"
+    "But you don’t even mind that the journey is so long because you are so entertained by the narrative!",
+
     "Did I mention the weather? It’s really beautiful. Sunny and warm.",
+
     "The grass around you is completely fresh. As you can see.",
-    "Well, you could see it if this wasn’t a text game. Just pretend then.",
+
+    "Well, you could see it if this wasn’t a text game.\n\nJust pretend then.",
+
+    "(IT Crowd reference)",
+
+    "Anyway...",
+
     "Left foot.",
+
     "Right foot.",
+
     "A little tip: You can read the text in Morgan Freeman’s voice for better mood.",
-    "Please do so and add me some points in the mood category later, otherwise it is your fault that you didn’t, so don’t blame me. Thanks.",
+
+    "Please do so and add me some points in the mood category later.\n\n"
+    "Otherwise it is your fault that you didn’t, so don’t blame me.\n\n"
+    "Thanks.",
+
     "You know what? We can meditate a bit!",
+
     "Let’s be quiet for some next steps.",
-    "…",
-    "…",
-    "Focus on your breath to calm your mind.",
-    "…",
-    "…",
+
+    "...",
+
+    "...",
+
+    "Focus on your breathing to calm your mind.",
+
+    "Inhale.",
+
+    "...",
+
+    "Exhale.",
+
+    "...",
+
+    "...",
+
     "If you played some of my older games, you know I also create shitty graphics for Ludum Dare games.",
+
     "But today there is no time for that. So you can have a shitty ASCI picture of the road you are walking on.",
     "Btw, the tree looked like this: (another ASCII art)",
     "I also create some crazy kazoo soundtracks.",
